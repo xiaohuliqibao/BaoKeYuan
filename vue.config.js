724cc0b606f76f8013bec6c422bb842176ff9b3e
xiaohuliqibao <xiaohuliqibao@gmail.com>
2021-01-10 22:46:24 +0800

    初始化项目

---
+++ vue.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+//transpileDependencies: ["vuetify"],
+/*
+  pluginOptions: {
+    i18n: {
+      locale: "zh",
+      fallbackLocale: "zh",
+      localeDir: "locales",
+      enableInSFC: true,
+    },
+  },
+*/
+  devServer: {
+    disableHostCheck: true,
+/*
+    proxy: {
+      "/voice": {
+        target: "http://47.102.112.85:9090",
+        changeOrigin: true,
+      },
+      "/miraibot": {
+        target: "http://47.102.112.85:777",
+        changeOrigin: true,
+      },
+    },
+    */
+  },
+};
